2c995fdef9a3372d558b3b09177b6cc34cd105c6
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-10 15:52:46 -0700

    add JS code to make voting links appear
    Co-authored-by: Keisha Hepner <kmhepner90@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,22 +5,24 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault();
 
-    // we access two HTML elements, the drink menu and the message to underaged folks,
+    // we access two HTML elements, the voting info and the message to underaged folks,
     // and add the hidden class to those elements;
     // doing this clears results before displaying new ones, which
     // allows the user to submit the form again and again, and
     // see new results.
-    let drinkMenu = document.getElementById("drinks");
-    drinkMenu.setAttribute("class", "hidden");
-    let under21Message = document.getElementById("under-21");
-    under21Message.setAttribute("class", "hidden");
+    let canVote = document.getElementById("canVote");
+    canVote.setAttribute("class", "hidden");
+    let cannotVote = document.getElementById("under-18");
+    cannotVote.setAttribute("class", "hidden");
 
     // we gather the age value
     const age = parseInt(document.querySelector("input#age").value);
 
-    // we check if the age is greater than 21.
-    if (age >= 21) {
-      drinkMenu.removeAttribute("class");
-    } 
+    // we check if the age is greater than 18.
+    if (age >= 18) {
+      canVote.removeAttribute("class");
+    } else {
+      cannotVote.removeAttribute("class");
+    }
   };
 };
